c263c6b2e914bf923123fb096eda05adb39c926b
HimeakLucas <lucasadeodato.oli@gmail.com>
2025-01-22 16:27:03 -0300

    Refactored project structure and added training loop

--- utils/frame_processing.py
+++ utils/frame_processing.py
@@ -18,5 +18,5 @@ def extract_y_channel_frames(y4m_video_path, output_dir):
 
 if __name__ == "__main__":
     video_path = "sequences/crowd_run_1080p50_60f.y4m" 
-    output_dir = "frames/video1/"
+    output_dir = "frames/video2/"
     extract_y_channel_frames(video_path, output_dir)
